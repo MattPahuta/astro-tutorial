ef0b3c1af06399376037f4f4cfb05c0d8e69c12c
Matt Pahuta <mhpahuta@gmail.com>
2024-01-25 16:28:23 -0800

    Add rss feed to blog

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Matt Haines Dev | Blog',
+    description: 'My journey learning Astro and beyond',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
